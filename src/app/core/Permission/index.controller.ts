3c86144af5e1d69a26d098ab9d775130d3e5d170
dangphu2412 <dangphu241299@gmail.com>
2020-12-09 20:59:03 +0700

    Fix internal server when create customer Error in map undefined permissions Permision no need in jwt payload so i remove it

--- src/app/core/Permission/index.controller.ts
+++ src/app/core/Permission/index.controller.ts
@@ -1,12 +1,16 @@
 import {ApiOperation, ApiTags} from "@nestjs/swagger";
 import {Controller, Get} from "@nestjs/common";
 import {PermissionService} from "./index.service";
+import {GrantAccess} from "src/common/decorators";
 
 @ApiTags("Permissions")
 @Controller("permissions")
 export class PermissionController {
   constructor(public service: PermissionService) {}
 
+  @GrantAccess({
+    action: "READ"
+  })
   @ApiOperation({
     summary: "Fetch all permissions"
   })
