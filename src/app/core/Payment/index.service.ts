52ddcdf5a46043e0484399c5148b52d39ba291e0
dangphu2412 <dangphu241299@gmail.com>
2020-12-09 21:47:28 +0700

    Comment condition in payment to test

--- src/app/core/Payment/index.service.ts
+++ src/app/core/Payment/index.service.ts
@@ -55,12 +55,12 @@ export class PaymentService extends TypeOrmCrudService<Payment> {
 
     switch (type) {
       case EPayment.PAY_OUT:
-        if (entity.amount > entity.bill.providerRemain) {
-          throw new ConflictException(
-            BillError.ConflictRemainGreaterThan,
-            ErrorCodeEnum.CONFLICT
-          )
-        }
+        // if (entity.amount > entity.bill.providerRemain) {
+        //   throw new ConflictException(
+        //     BillError.ConflictRemainGreaterThan,
+        //     ErrorCodeEnum.CONFLICT
+        //   )
+        // }
         entity.bill.providerRemain -= entity.amount;
         if (entity.bill.providerRemain === 0) {
           entity.bill.status = BillStatus.CUSTOMER_PAID
@@ -68,12 +68,12 @@ export class PaymentService extends TypeOrmCrudService<Payment> {
         break;
       case EPayment.GET_IN:
       default:
-        if (entity.amount > entity.bill.providerRemain) {
-          throw new ConflictException(
-            BillError.ConflictRemainGreaterThan,
-            ErrorCodeEnum.CONFLICT
-          )
-        }
+        // if (entity.amount > entity.bill.providerRemain) {
+        //   throw new ConflictException(
+        //     BillError.ConflictRemainGreaterThan,
+        //     ErrorCodeEnum.CONFLICT
+        //   )
+        // }
         entity.bill.customerRemain -= entity.amount;
         if (entity.bill.customerRemain === 0) {
           entity.bill.status = BillStatus.PROVIDER_PAID;
