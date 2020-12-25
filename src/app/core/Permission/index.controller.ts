5ef660ef5676a0e119571eaff4e9e1e9eef3b494
dangphu2412 <dangphu241299@gmail.com>
2020-12-25 16:46:17 +0700

    Add feature decorator in permission api

--- src/app/core/Permission/index.controller.ts
+++ src/app/core/Permission/index.controller.ts
@@ -2,7 +2,10 @@ import {ApiOperation, ApiTags} from "@nestjs/swagger";
 import {Controller, Get} from "@nestjs/common";
 import {PermissionService} from "./index.service";
 import {GrantAccess} from "src/common/decorators";
+import {Feature} from "@nestjsx/crud";
+import {ECrudFeature} from "src/common/enums";
 
+@Feature(ECrudFeature.PERMISSIONS)
 @ApiTags("Permissions")
 @Controller("permissions")
 export class PermissionController {
