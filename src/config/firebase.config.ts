52d5635eed3f6bdf9ebf1d2942cc463e53ea0bca
dangphu2412 <dangphu241299@gmail.com>
2021-01-23 09:21:45 +0700

    v1.3: Edit configuration firebase
    - No depend on firebase.json

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,8 +1,7 @@
-import {credential, ServiceAccount, AppOptions} from "firebase-admin";
+import {AppOptions} from "firebase-admin";
 import {FIREBASE_CONFIG} from "../env";
-import serviceAccount from "./firebase.json";
 
 export const firebaseConfig: AppOptions = {
   databaseURL: FIREBASE_CONFIG.DATABASE_FIREBASE_URL,
-  credential: credential.cert(serviceAccount as ServiceAccount)
+  projectId: FIREBASE_CONFIG.PROJECT_ID
 };
